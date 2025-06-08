1b134920fc7c97d76a73e889a180aa24075c8292
James Hodgkinson <james@terminaloutcomes.com>
2025-06-08 16:59:44 +1000

    maint: adding more features

--- obd2_play/stats.py
+++ obd2_play/stats.py
@@ -9,10 +9,12 @@ from obd2_play import startup
 
 @click.command()
 @click.option("-d", "--debug", is_flag=True, help="Enable debug logging")
+@click.option("--everything", is_flag=True, help="Show all available commands")
+@click.option("--once", is_flag=True, help="Run once and exit")
 @click.option(
     "-s", "--show-commands", is_flag=True, help="Show available commands and quit"
 )
-def main(debug: bool, show_commands: bool) -> None:
+def main(debug: bool, show_commands: bool, everything: bool, once: bool) -> None:
     connection = startup(debug)
     if connection is None:
         logger.error("Couldn't connect")
@@ -30,10 +32,24 @@ def main(debug: bool, show_commands: bool) -> None:
         "ELM_VOLTAGE": "ELM_VOLTAGE",
     }
 
+    if everything:
+        sleep_time = 5
+    else:
+        sleep_time = 1
+
     while True:
-
-        for key, value in wanted_commands.items():
-            if key in available_commands:
-                logger.info(f"{key}: {connection.query(getattr(obd.commands, value))}")
-
-        sleep(1)
+        if everything:
+            for command in available_commands:
+                logger.info("{} - {}", command, connection.query(command))
+            logger.success("All commands displayed")
+        else:
+            for key, value in wanted_commands.items():
+                if key in available_commands:
+                    logger.info(
+                        f"{key}: {connection.query(getattr(obd.commands, value))}"
+                    )
+                else:
+                    logger.debug(f"{key} not supported by the vehicle")
+        if once:
+            break
+        sleep(sleep_time)
